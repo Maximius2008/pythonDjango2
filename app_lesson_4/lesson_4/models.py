68b1f5b9cf67fd604e1a651a953370ccb86d7bdf
Maximius2008 <krutoikrasavec@gmail.com>
2023-08-02 16:01:06 +0300

    добавлен класс Meta

--- app_lesson_4/lesson_4/models.py
+++ app_lesson_4/lesson_4/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Advertisement(models.Model):
+
+    title = models.CharField(verbose_name='Название', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметьте, если уместен торг')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'advertisements'
+
+'''DecimalField - поле для чисел;
+    CharField - поле для маленьких строк;      
+    TextField - поле для больших строк;
+    BooleanField - да или нет;
+'''
+
+
+"""
+название
+описание
+цена
+время создания
+время обновления
+торг
+"""
