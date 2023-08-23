bee098ef8755d76143279693c8117109a0d9ec50
Maximius2008 <krutoikrasavec@gmail.com>
2023-08-23 11:45:55 +0300

    исправлена ошибка с бесконечной рекурсией
    вместо app_lesson_4.urls надо было lesson_4.urls

--- app_lesson_4/app_lesson_4/urls.py
+++ app_lesson_4/app_lesson_4/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_lesson_4.urls')),
+    path('', include('lesson_4.urls')),
 ]
 
 if settings.DEBUG: # if ... == True
